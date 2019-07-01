6a647acc1ba585c7a65244148beb6f2ad8f32f78
Aviv <vaknins33@gmail.com>
2019-07-01 11:08:03 +0300

    Updates

---
+++ precache-manifest.4b298779bda9ef8297832eeb7f62a263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c36e8ce09614a16ef99dcbf0c102bb1",
+    "url": "/supportnotifs/index.html"
+  },
+  {
+    "revision": "d3b21c90ad2402aacb5c",
+    "url": "/supportnotifs/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "48781bc61ca42b2c5e1a",
+    "url": "/supportnotifs/static/css/main.59853cea.chunk.css"
+  },
+  {
+    "revision": "d3b21c90ad2402aacb5c",
+    "url": "/supportnotifs/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "48781bc61ca42b2c5e1a",
+    "url": "/supportnotifs/static/js/main.f32f3828.chunk.js"
+  },
+  {
+    "revision": "31463ee1cc43363f7c0a",
+    "url": "/supportnotifs/static/js/runtime~main.d3ca578f.js"
+  }
+]);
