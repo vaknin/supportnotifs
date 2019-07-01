0ca9d900ae95009c404611229fa010000ac0964c
Aviv <vaknins33@gmail.com>
2019-07-01 13:53:44 +0300

    Updates

--- precache-manifest.6ece0174c049c5470ccd25b33722a5e3.js
+++ precache-manifest.6ece0174c049c5470ccd25b33722a5e3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc133fc7f128d96ceea5a73bfb0f3760",
+    "revision": "815cf33a7c227143572b8827e6a7a581",
     "url": "/supportnotifs/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/supportnotifs/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "0afa2bb71849f60d16c6",
-    "url": "/supportnotifs/static/css/main.61db7f87.chunk.css"
+    "revision": "21ccf0f23c86bbbfca51",
+    "url": "/supportnotifs/static/css/main.ea4a36ed.chunk.css"
   },
   {
     "revision": "d3b21c90ad2402aacb5c",
     "url": "/supportnotifs/static/js/2.baf3c1c8.chunk.js"
   },
   {
-    "revision": "0afa2bb71849f60d16c6",
-    "url": "/supportnotifs/static/js/main.713bdb87.chunk.js"
+    "revision": "21ccf0f23c86bbbfca51",
+    "url": "/supportnotifs/static/js/main.65f8d099.chunk.js"
   },
   {
     "revision": "31463ee1cc43363f7c0a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/supportnotifs/precache-manifest.9d06c482326622d1e540f28f0ea138b4.js"
+  "/supportnotifs/precache-manifest.6ece0174c049c5470ccd25b33722a5e3.js"
 );
 
 self.addEventListener('message', (event) => {
